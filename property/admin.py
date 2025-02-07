531cbc67a8ae577c0d976a1105a993cfd8db3042
gardnervile <lordy1jo@gmail.com>
2025-02-07 21:30:44 +0500

    Normalize Flat.phonenumbers into Flat.owner_pure_phone.

--- property/admin.py
+++ property/admin.py
@@ -5,7 +5,7 @@ from .models import Flat, Complaint
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('owner','town','address',)
     readonly_fields = ['created_at']
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owner_pure_phone', 'owners_phonenumber')
     list_editable = ('new_building',)
     list_filter = ('new_building',)
     raw_id_fields = ('liked_by',)
---
+++ property/migrations/0009_normalize_phone_numbers.py
@@ -0,0 +1,29 @@
+from django.db import migrations
+import phonenumbers
+
+
+def normalize_phone_numbers(apps, schema_editor):
+    Flat = apps.get_model('property', 'Flat')
+
+    for flat in Flat.objects.all():
+        if flat.owners_phonenumber:
+            try:
+                parsed_number = phonenumbers.parse(flat.owners_phonenumber, "RU")
+                if phonenumbers.is_valid_number(parsed_number):
+                    flat.owner_pure_phone = phonenumbers.format_number(
+                        parsed_number, phonenumbers.PhoneNumberFormat.E164
+                    )
+                    flat.save(update_fields=['owner_pure_phone'])
+            except phonenumbers.NumberParseException:
+                pass 
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('property', '0008_auto_20250206_2212'),  # Подставьте имя последней миграции
+    ]
+
+    operations = [
+        migrations.RunPython(normalize_phone_numbers),
+    ]
