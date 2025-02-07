cc94de5d0b49f89735b5a8e465ac46efa45952b2
gardnervile <lordy1jo@gmail.com>
2025-02-07 22:37:01 +0500

    added inline

--- property/admin.py
+++ property/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class FlatsInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ['owner']
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('owner','town','address',)
     readonly_fields = ['created_at']
@@ -9,15 +14,19 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ('new_building',)
     list_filter = ('new_building',)
     raw_id_fields = ('liked_by',)
+    inlines = [FlatsInline,]
+    exclude = ['flats']
 
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('user','flat')
 
+
 class OwnerAdmin(admin.ModelAdmin):
     list_display = ('owner', 'owners_phonenumber', 'owner_pure_phone')
     raw_id_fields = ('flats',)
     readonly_fields = ('owner_pure_phone',)
+
 
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
