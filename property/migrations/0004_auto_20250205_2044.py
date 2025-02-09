26cefc140acde702bf6e108f26c460ce3089f372
gardnervile <lordy1jo@gmail.com>
2025-02-09 18:08:27 +0500

    Optimize new_building

--- property/migrations/0004_auto_20250205_2044.py
+++ property/migrations/0004_auto_20250205_2044.py
@@ -3,18 +3,15 @@
 from django.db import migrations
 
 def fill_new_building(apps, schema_editor):
-    Flat = apps.get_model('property', 'Flat')  # Замените 'property' на имя вашего приложения
-    for flat in Flat.objects.all():
-        if flat.construction_year and flat.construction_year >= 2015:
-            flat.new_building = True
-        else:
-            flat.new_building = False
-        flat.save()
+    Flat = apps.get_model('property', 'Flat')
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
+    Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
+
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('property', '0003_flat_new_building'),  # Укажите правильный файл предыдущей миграции
+        ('property', '0003_flat_new_building'),
     ]
 
     operations = [
